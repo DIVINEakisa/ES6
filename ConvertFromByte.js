96c893b802f0efcf7444a9ad6c84f702cd428dd5
DIVINEakisa <akisadivine11@gmail.com>
2025-10-17 14:11:08 +0200

    Add Description(Question)

--- ConvertFromByte.js
+++ ConvertFromByte.js
@@ -1,3 +1,5 @@
+//write the function that will return the given byte into its original number
+// Example: 111 will return 7
 let convertIntoBytes = (byte) => {
   let originalNumber = parseInt(byte, 2);
   return originalNumber;
