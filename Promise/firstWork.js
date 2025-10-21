c65278b53d9b9c60ac8262c330b23acac9a6e2de
DIVINEakisa <akisadivine11@gmail.com>
2025-10-21 17:56:08 +0200

    simulating a small online store

---
+++ Promise/firstWork.js
@@ -0,0 +1,10 @@
+// ng with Promises in JavaScript
+// Question:
+
+// You are simulating a small online store.
+// Create a Promise-based system that performs the following tasks in order:
+
+// Fetch Product Data (2 seconds delay):
+// Simulate fetching a product from a database. The product has:
+
+// { id: 101, name: "Wireless Mouse", price: 25000 }
