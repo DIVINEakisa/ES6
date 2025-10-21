69eba4df29146ffd1150d3ec3a9a319f40aafeea
DIVINEakisa <akisadivine11@gmail.com>
2025-10-21 18:02:28 +0200

    returns a Promise Simulate a 2-second delay using setTimeout

---
+++ Promise/processPayment.js
@@ -0,0 +1,15 @@
+// Create a function processPayment(orderMessage)
+
+// It returns a Promise.
+
+// Simulate a 2-second delay using setTimeout.
+
+// Resolve with:
+// 👉 "Payment processed successfully for: <orderMessage>"
+
+// Reject randomly sometimes to simulate payment failure.
+function processPayment(orderMessage) {
+  return new Promise((resolve, reject) => {
+    orderMessage.setTimeout(() => {}, timeout);
+  });
+}
