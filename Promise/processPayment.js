abbef3424fbd9c5abd47b003cd76c89d8704ce27
DIVINEakisa <akisadivine11@gmail.com>
2025-10-23 19:57:49 +0200

    Add Process Payment

--- Promise/processPayment.js
+++ Promise/processPayment.js
@@ -1,15 +1,19 @@
-// Create a function processPayment(orderMessage)
-
-// It returns a Promise.
-
-// Simulate a 2-second delay using setTimeout.
-
-// Resolve with:
-// 👉 "Payment processed successfully for: <orderMessage>"
-
-// Reject randomly sometimes to simulate payment failure.
 function processPayment(orderMessage) {
   return new Promise((resolve, reject) => {
-    orderMessage.setTimeout(() => {}, timeout);
+    setTimeout(() => {
+      let success = Math.random() > 0.03;
+      if (success) {
+        resolve(`Payment processed successfully for: ${orderMessage}`);
+      } else {
+        reject(`Payment processed unsuccessfully for: ${orderMessage}`);
+      }
+    }, 2000);
   });
 }
+processPayment("hell")
+  .then((result) => {
+    console.log("Payment Success" + result);
+  })
+  .catch((error) => {
+    console.log("There's Something Wrong" + error);
+  });
