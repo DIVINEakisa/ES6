7b1c8e4133831f17328c8026759db398abd4b75a
DIVINEakisa <akisadivine11@gmail.com>
2025-10-23 19:58:16 +0200

    Add Description

--- Promise/processPayment.js
+++ Promise/processPayment.js
@@ -1,3 +1,13 @@
+// Create a function processPayment(orderMessage)
+
+// It returns a Promise.
+
+// Simulate a 2-second delay using setTimeout.
+
+// Resolve with:
+// 👉 "Payment processed successfully for: <orderMessage>"
+
+// Reject randomly sometimes to simulate payment failure.
 function processPayment(orderMessage) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
