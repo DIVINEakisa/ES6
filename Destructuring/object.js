9f07796cf42bf16fb8ff1d9c42b68cdd9d465436
DIVINEakisa <akisadivine11@gmail.com>
2025-10-28 17:31:39 +0200

    add Object destructuring

---
+++ Destructuring/object.js
@@ -0,0 +1,10 @@
+let ObjectDestr = (student) => {
+  student = {
+    name: "Dede",
+    age: 20,
+    course: "IT",
+  };
+  let { name, course } = student;
+  return { name, course };
+};
+console.log(ObjectDestr());
